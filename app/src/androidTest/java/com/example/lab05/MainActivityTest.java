5e02e0488d72f18a3db48ac66e90eb760c219f60
Manisha <mani0076@algonquinlive.com>
2024-03-29 15:16:42 -0400

    Final

--- app/src/androidTest/java/com/example/lab05/MainActivityTest.java
+++ app/src/androidTest/java/com/example/lab05/MainActivityTest.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import static androidx.test.InstrumentationRegistry.getInstrumentation;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
